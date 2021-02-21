e3591dcd38761ea172c4603949c60e8163663262
Danil <dddsaltykov@gmail.com>
2021-02-21 20:50:14 +0300

    step 1

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$('.company__columns').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: $('.prevArrow'),
+    nextArrow: $('.nextArrow')
+  });
