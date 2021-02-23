1aefb3309c63a2fc79e81afff09ef017ce57c9b6
Danil <dddsaltykov@gmail.com>
2021-02-23 13:02:34 +0300

    full version desktop

--- js/script2.js
+++ js/script2.js
@@ -1,11 +1,33 @@
-let btn = document.querySelector('.questions__button');
+
 let questionText = document.querySelector('.questions__text');
 
 
-btn.addEventListener('click', function(){
-    if(questionText.style.display == 'none'){
-        questionText.style.display = 'block';
-    } else {
-        questionText.style.display = 'none';
-    }
-})
+
+let btn = document.getElementsByClassName("questions__button");
+
+
+for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+
+    /* Toggle between hiding and showing the active panel */
+    let panel = this.previousElementSibling;
+    if (panel.style.display === "none") {
+      panel.style.display = "block";
+} else {
+      panel.style.display = "block";
+}
+
+
+
+
+if (panel.style.maxHeight) {
+    panel.style.maxHeight = null;
+} else {
+panel.style.maxHeight = panel.scrollHeight + 10 + "px";
+}
+});
+}
